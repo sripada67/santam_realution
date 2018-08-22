9aba2fb565f251841693522267ffb76d0641002e
viswanadh <viswanadh.sripada@gmail.com>
2018-08-22 17:54:39 +0530

    docker test

---
+++ print_env.sh
@@ -0,0 +1,1 @@
+printenv | sed 's/^\(.*\)$/export \1/g' > /var/www/html/santam/print_env.sh
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 php artisan migrate
+#Extra line added in the script to run all command line arguments
+exec "$@";
+
